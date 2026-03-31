beaaceac477b46c1bc259913d0196ba32260f3fa
Quigley Malcolm <tqmalcolm@protonmail.com>
2026-03-31 01:23:55 -0700

    Add publishing details to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule DiceLib.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "DiceLib",
+      source_url: "https://github.com/TTRPG-Dev/dice_lib",
     ]
   end
 
@@ -23,6 +27,17 @@ defmodule DiceLib.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description do
+    "DiceLib is a library for rolling dice. Is it the best? Eh, it's a roll of the dice"
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/TTRPG-Dev/dice_lib"}
     ]
   end
 end
